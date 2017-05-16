e799443ec4368a052d2624d9e928dd3397f14616
Ioannis Sofos <ioannis.sofos@ext.jrc.ec.europa.eu>
2017-05-16 16:43:28 +0200

    restructure API

--- services/api.service.ts
+++ services/api.service.ts
@@ -17,14 +17,107 @@ export class GsHttpApiService {
     // this.api_url = environment.api_url;
   }
 
+  public setBaseUrl(url: string) {
+    this.api_url = url;
+  }
+
   private getText(response: Response) {
     return response.text();
   }
 
-  public get(path: string): Observable<any> {
-    return this.http.get(`${this.api_url}${path}`, { headers: this.headers })
-      .catch(err => Observable.throw(err))
-      .map(this.getText);
+
+  private getJson(response: Response) {
+    //  console.log(response)
+    return response.json();
+  }
+
+
+  private checkForError(response: Response): Response | Observable<any> {
+    // console.log(response)
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      let error = new Error(response.statusText)
+      error['response'] = response;
+      console.error(error);
+      throw error;
+    }
+  }
+
+
+
+  public httpGET(path: string, responseType: 'text' | 'json' = 'text') {
+
+    switch (responseType) {
+      case 'text':
+        return this.http.get(`${this.api_url}${path}`, { headers: this.headers })
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getText).toPromise();
+      case 'json':
+        return this.http.get(`${this.api_url}${path}`, { headers: this.headers })
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getJson).toPromise();
+    }
+
+  }
+
+
+
+  public httpPOST(path: string, body: any, responseType: 'text' | 'json' = 'text') {
+
+    switch (responseType) {
+      case 'text':
+        return this.http.post(
+          `${this.api_url}${path}`,
+          JSON.stringify(body),
+          { headers: this.headers }
+        )
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getText).toPromise();
+      case 'json':
+        return this.http.post(
+          `${this.api_url}${path}`,
+          JSON.stringify(body),
+          { headers: this.headers }
+        )
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getJson).toPromise();
+    }
+
+  }
+
+
+
+  httpDELETE(path: string, responseType: 'text' | 'json' = 'text') {
+
+    switch (responseType) {
+      case 'text':
+        return this.http.delete(
+          `${this.api_url}${path}`,
+          { headers: this.headers }
+        )
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getText).toPromise();
+      case 'json':
+        return this.http.delete(
+          `${this.api_url}${path}`,
+          { headers: this.headers }
+        )
+          .map(this.checkForError)
+          .catch(err => Observable.throw(err))
+          .map(this.getJson).toPromise();
+    }
+
+  }
+
+
+  public setHeaders(headers: any) {
+    Object.keys(headers).forEach(header => this.headers.set(header, headers[header]));
   }
 
 }
